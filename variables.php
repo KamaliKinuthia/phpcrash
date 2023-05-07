a559974c9e46938e49dfc84c17f9525f1389aa28
Kinuthia <119793890+KamaliKinuthia@users.noreply.github.com>
2023-05-07 10:00:18 +0300

    php crash course

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+//data types
+$name='Brad';//string
+$age=40;//integer
+$has_kids= true;//boolean 
+
+ //echo $name, $has_kids;
+ //var_dump($has_kids,$name)
+//echo $name .'is' .$age.' years old';
+//echo "$name is $age years old"
+
+// $x='5'+'5';
+// var_dump($x);
+// echo 10-5;
+// echo 5*6;
+// echo 10/2;
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
+
+
+
+
+
+?>
